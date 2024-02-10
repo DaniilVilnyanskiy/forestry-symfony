97a0a9499b3bea310d94a71a0162a18b6bdc1d79
Daniil Vilnyanskiy <103027487+DaniilVilnyanskiy@users.noreply.github.com>
2024-02-10 18:55:51 +0400

    feat: Add AppFixtures

--- app/src/DataFixtures/AppFixtures.php
+++ app/src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Product;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +10,12 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        for ($i = 0; $i < 10; $i++) {
+          $product = new Product();
+          $product->setTitle('product title '.$i);
+          $product->setDescription('Description of product');
+          $manager->persist($product);
+        }
 
         $manager->flush();
     }
